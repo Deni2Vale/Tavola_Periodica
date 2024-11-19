f84baf3eb7fd0074f86b735abfd0c798de70c1ff
Deni2Vale <denise.nacional@iismontalegenova.it>
2024-11-19 12:39:42 +0100

    YAS WHAHAHA

--- programma.js
+++ programma.js
@@ -1,37 +1,36 @@
 
-let Idrogeno = {};
-
-Idrogeno.simbolo = 'H'
-Idrogeno.Z = 1;
-Idrogeno.nome = 'Idrogeno'
-
-let Elio = {};
-
-Elio.simbolo = 'He'
-Elio.Z = 2;
-Elio.nome = 'Elio'
-
-
-let elementi = [Idrogeno, Elio];
 
 for (const element of elementi) {
 let han = document.getElementById('han');
+han.style.position = 'relative';
 
 
 let yoojin = document.createElement('div');
 yoojin.style.borderStyle = 'solid';
-yoojin.style.width = '90px'
+yoojin.style.borderWidth = '0.5px'
+yoojin.style.width = '95px'
+yoojin.style.height = '125px';
+yoojin.style.position = 'absolute';
+yoojin.style.left = element.xpos * 95 + 'px';
+yoojin.style.top = element.ypos * 125 + 'px';
+yoojin.style.backgroundColor = '#' + element['cpk-hex'];
+yoojin.style.textAlign = 'center';
+yoojin.style.boxSizing = 'border-box';
 
 let Z = document.createElement('p');
-Z.innerText = element.Z;
+Z.innerText = element.number;
 yoojin.appendChild(Z);
 
 let simbolo = document.createElement('a');
-simbolo.innerText = element.simbolo;
+simbolo.innerText = element.symbol;
+simbolo.href = element.source;
 yoojin.appendChild(simbolo);
+simbolo.style.fontSize = '25px'
+simbolo.style.fontWeight = 'bold';
+simbolo.style.color = 'black';
 
 let nome = document.createElement('p');
-nome.innerText = element.nome;
+nome.innerText = element.name;
 yoojin.appendChild(nome);
 
 han.appendChild(yoojin);
